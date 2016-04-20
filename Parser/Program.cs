f05f9da89d152756869bc55d46248f9909a8eb7c
diamed <apollondiamed@gmail.com>
2016-04-20 18:52:32 +0500

    Added simple description and construction 'switch'

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;   //need to run process
+using System.Net;           //need to work with web
+using System.Threading;     //need to wait
 
 namespace Parser
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Enter the number that you want to parse:\n" +
+                                "1)Aimp skins\n" +
+                                "2)Certificates on Geekbrains\n");
+            switch(Console.Read()){
+                case (1):
+                    break;
+                case (2):
+                    break;
+                default:
+                    Console.WriteLine("You specify an invalid command. Please try again.");
+                    break;
+            }
+
+            
         }
     }
 }
